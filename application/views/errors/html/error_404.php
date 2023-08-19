c4f06fcd45b429e123e84dfa2ee2c338b76b28d1
Rizki Ramadhan <66109920+korizki@users.noreply.github.com>
2023-08-19 08:58:24 +0800

    feat: Adding custom error 404 restricted access page

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -1,64 +1,40 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+	defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>404 Page Not Found</title>
-<style type="text/css">
-
-::selection { background-color: #E13300; color: white; }
-::-moz-selection { background-color: #E13300; color: white; }
-
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
-
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
-
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
-
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
-
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
-}
-
-p {
-	margin: 12px 15px 12px 15px;
-}
+<style>
+	@import url("https://fonts.googleapis.com/css2?family=Inter:wght@200;400;600&display=swap");
+	* {
+		box-sizing: border-box;
+		font-family: "Inter", sans-serif;
+		padding: 0;
+		color: #27374d;
+		line-height: 1 !important;
+		margin: 0;
+	}
+	.box{
+		width: 100%;
+		position: absolute;
+		height: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.inner>img{
+		margin-top: 3em;
+	}
+	.inner{
+		text-align: center;
+	}
+	.inner>h1{
+		color: #0C356A;
+		font-size: 3em;
+		margin-bottom: 0.3em;
+	}
 </style>
-</head>
-<body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
+<div class="box">
+	<div class="inner">
+		<h1>Restricted Access</h1>
+		<p>Opss, sorry you don't have access to this page. Please <a href="#" onclick="history.back()">go back</a>.</p>
+		<img src="assets/404.png" alt="illustration" width="300">
 	</div>
-</body>
-</html>
+</div>
